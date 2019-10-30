220f6a18830fa2f4999f183c3717388a6950253b
hykim1126 <hykim@quescompany.com>
2019-10-30 11:46:36 +0900

    NFC option

--- myProject.ino
+++ myProject.ino
@@ -1,26 +1,31 @@
 #include <SoftwareSerial.h>
-#include <IRremote.h>
 #include <ESP8266.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_PN532.h>
+ 
 #define BT_RXD 2 
 #define BT_TXD 3
-#define LED_RED 8
-#define LED_GREEN 9
 #define RECV_PIN  12
 #define SSID        "SK_WiFiGIGA4A0F"
 #define PASSWORD    "ques1234@"
 #define HOST_NAME   "192.168.35.190"
 #define HOST_PORT   (3000)
-SoftwareSerial ESP_wifi(BT_RXD, BT_TXD);
-IRrecv irrecv(RECV_PIN);
-ESP8266 wifi(ESP_wifi);
+#define PN532_IRQ   (9)
+#define PN532_RESET (8)
 
-decode_results results;
+SoftwareSerial ESP_wifi(BT_RXD, BT_TXD);
+
+ESP8266 wifi(ESP_wifi);
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+
 String password = "";
+int OMC = 0;
 
 void setup() {
+  // WiFi setup
   bool success = false;
   Serial.begin(9600);
-  irrecv.enableIRIn();
   Serial.print("Configuring ESP8266..");
   do {
     success = wifi.joinAP(SSID, PASSWORD);
@@ -38,70 +43,60 @@ void setup() {
      while (1);
   }
   Serial.println("[ Setup end ]");
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
+  // NFC setup
+  Serial.print("\r\n\r\nPN532 NFC Start..");
+  nfc.begin();
+  
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata)
+  {
+      Serial.print("\r\nDidn't find PN53x board");
+      while (1); // halt
+  }
+  Serial.print("\r\nFound chip PN5");
+  Serial.print((versiondata>>24) & 0xFF, HEX);
+  Serial.print("\r\nFirmware ver. ");
+  Serial.print((versiondata>>16) & 0xFF, DEC);
+  Serial.print('.');
+  Serial.print((versiondata>>8) & 0xFF, DEC);
+
+  nfc.setPassiveActivationRetries(0xFF);
+
+  nfc.SAMConfig();
+
+ 
 } 
 void loop() {
-  digitalWrite(LED_RED, LOW);
-  digitalWrite(LED_GREEN, LOW);
+  bool success = false;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 }; 
+  uint8_t uidLength;
+  
+  Serial.print("\r\nWaiting for an ISO14443A card.");
 
-  Serial.println("Input password.");
-  while(results.value!=0x00FF906F){
-    if(irrecv.decode(&results)){
-      if(results.decode_type == NEC){
-        switch(results.value){
-          case 0x00FF6897:
-            Serial.println("Press '0'");
-            password +='0';
-            break;
-          case 0x00FF30CF:
-            Serial.println("Press '1'");
-            password +='1';
-            break;
-          case 0x00FF18E7:
-            Serial.println("Press '2'");
-            password +='2';
-            break;
-          case 0x00FF7A85:
-            Serial.println("Press '3'");
-            password +='3';
-            break;
-          case 0x00FF10EF:
-            Serial.println("Press '4'");
-            password +='4';
-            break;
-          case 0x00FF38C7:
-            Serial.println("Press '5'");
-            password +='5';
-            break;
-          case 0x00FF5AA5:
-            Serial.println("Press '6'");
-            password +='6';
-            break;
-          case 0x00FF42BD:
-            Serial.println("Press '7'");
-            password +='7';
-            break;
-          case 0x00FF4AB5:
-            Serial.println("Press '8'");
-            password +='8';
-            break;                          
-          case 0x00FF52AD:
-            Serial.println("Press '9'");
-            password +='9';
-            break;                          
-          case 0x00FF906F:
-            Serial.println("Complete.");
-            break;
-          default:
-            break;
-        }
-      }
-    irrecv.resume();
-    }   
+  while(!success){
+    success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength); 
+    Serial.print(".");
+    if(OMC){
+      break;
+    }
   }
-  Serial.println("password : " + password);
 
+  OMC = 1;
+  
+  Serial.print("\r\nFound a card!");
+  Serial.print("\r\nUID Length: ");
+  Serial.print(uidLength, DEC);
+  Serial.print(" bytes");
+  Serial.print("\r\nUID Value: ");
+
+  for (uint8_t i = 0; i < uidLength; i++)
+  {
+      Serial.print(" 0x");
+      Serial.print(uid[i], HEX);
+      password += (int)uid[i];
+  }
+  Serial.println("\r\n"+ password);
+  
 
   uint8_t buffer[7]={0};
   String judge = "";
@@ -110,6 +105,7 @@ void loop() {
   stl += "\r\n\r\n";
   char* cmd = new char[stl.length()+1];
   strcpy(cmd, stl.c_str());
+  Serial.println(cmd);
   if(wifi.send(cmd, strlen(cmd))){
     int len = wifi.recvMP(buffer,300,10000);
     if(len > 0){
@@ -133,17 +129,8 @@ void loop() {
   delete[] cmd;
   cmd = NULL;
   password = "";
-  results.value = "";
+  OMC = 0;
   Serial.println(judge);
-  if(judge.equals("_passed")){
-    digitalWrite(LED_GREEN, HIGH);
-    Serial.println("Access alowed!");
-  } else if(judge.equals("_banned")){
-    digitalWrite(LED_RED, HIGH);
-    Serial.println("Access banned!");
-  } else {
-  Serial.println("An error occured!");
-  }
-  delay(300);
+
   wifi.releaseTCP();
 }
